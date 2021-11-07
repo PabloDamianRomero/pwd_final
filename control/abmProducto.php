ec36bb7c6256d2e11189aac5dc4c5b8878f6cc98
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-07 02:13:36 -0300

    abmProducto actualizado

--- control/abmProducto.php
+++ control/abmProducto.php
@@ -1,24 +1,129 @@
 <?php
-class abmProducto{
+class abmProducto
+{
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves coinciden
+     * con los nombres de las variables instancias del objeto
+     * @param array $param
+     * @return Producto
+     */
+    private function cargarObjeto($param)
+    {
+        $obj = null;
+        if (array_key_exists('idproducto', $param) and array_key_exists('pronombre', $param) and array_key_exists('prodetalle', $param)
+            and array_key_exists('procantstock', $param)) {
+            $obj = new Producto();
+            $obj->setear($param['idproducto'], $param['pronombre'], $param['prodetalle'], $param['procantstock']);
+        }
+        return $obj;
+    }
+
+    /**
+     * Espera como parametro un arreglo asociativo donde las claves 
+     * coinciden con los nombres de las variables instancias del objeto que son claves
+     * @param array $param
+     * @return Producto
+     */
+    private function cargarObjetoConClave($param)
+    {
+        $obj = null;
+        if (isset($param['idproducto'])) {
+            $obj = new Producto();
+            $obj->setear($param['idproducto'], null, null, null);
+        }
+        return $obj;
+    }
+
+    /**
+     * Corrobora que dentro del arreglo asociativo estan seteados los campos claves
+     * @param array $param
+     * @return boolean
+     */
+    private function seteadosCamposClaves($param)
+    {
+        $resp = false;
+        if (isset($param['idproducto']))
+            $resp = true;
+        return $resp;
+    }
+
+    /**
+     * Carga un objeto con los datos pasados por parámetro y lo 
+     * Inserta en la base de datos
+     * @param array $param
+     * @return boolean
+     */
+    public function alta($param)
+    {
+        $resp = false;
+        $elObjtProducto = $this->cargarObjeto($param);
+        if ($elObjtProducto != null and $elObjtProducto->insertar()) {
+            $resp = true;
+        }
+        return $resp;
+    }
+
+    /**
+     * 
+     * @param array $param
+     * @return boolean
+     */
+    public function baja($param)
+    {
+        $resp = false;
+        if ($this->seteadosCamposClaves($param)) {
+            $elObjtProducto = $this->cargarObjetoConClave($param);
+            if ($elObjtProducto != null and $elObjtProducto->eliminar()) {
+                $resp = true;
+            }
+        }
+        return $resp;
+    }
+
+    /**
+     * Carga un obj con los datos pasados por parámetro y lo modifica en base de datos (update)
+     * @param array $param
+     * @return boolean
+     */
+    public function modificacion($param)
+    {
+        $resp = false;
+        if ($this->seteadosCamposClaves($param)) {
+            $elObjtProducto = $this->cargarObjeto($param);
+            if ($elObjtProducto != null and $elObjtProducto->modificar()) {
+                $resp = true;
+            }
+        }
+        return $resp;
+    }
+
     /**
      * permite buscar un objeto
      * @param array $param
      * @return boolean
      */
-    public function buscar($param){
+    public function buscar($param)
+    {
         $where = " true ";
-        if ($param<>NULL){
-            if  (isset($param['idproducto']))
-                $where.=" and idproducto =".$param['idproducto'];
-            if  (isset($param['pronombre']))
-                 $where.=" and pronombre ='".$param['pronombre']."'";
-            if  (isset($param['prodetalle']))
-            $where.=" and prodetalle ='".$param['prodetalle']."'";
-            if  (isset($param['cantstock']))
-            $where.=" and cantstock ='".$param['cantstock']."'";
+        if ($param != null) {
+            if (isset($param['idproducto'])) {
+                $where .= " and idproducto =" . $param['idproducto'];
+            }
+
+            if (isset($param['pronombre'])) {
+                $where .= " and pronombre ='" . $param['pronombre'] . "'";
+            }
+
+            if (isset($param['prodetalle'])) {
+                $where .= " and prodetalle ='" . $param['prodetalle'] . "'";
+            }
+
+            if (isset($param['procantstock'])) {
+                $where .= " and procantstock ='" . $param['procantstock'] . "'";
+            }
+
         }
-        $arreglo = Producto::listar($where);  
+        $arreglo = Producto::listar($where);
         return $arreglo;
     }
 }
-?>
