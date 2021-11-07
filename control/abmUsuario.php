34c922f846d4eec1a87375680d010968ccb64b04
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-07 02:03:25 -0300

    abmUsuario actualizado

--- control/abmUsuario.php
+++ control/abmUsuario.php
@@ -1,125 +1,143 @@
 <?php
-class abmUsuario{
+class abmUsuario
+{
     /**
      * Espera como parametro un arreglo asociativo donde las claves coinciden con los nombres de las variables instancias del objeto
      * @param array $param
      * @return Usuario
      */
-    private function cargarObjeto($param){
+    private function cargarObjeto($param)
+    {
         $obj = null;
-           
-        if( array_key_exists('id',$param) and array_key_exists('descript',$param)){
+        if (array_key_exists('idusuario', $param) and array_key_exists('usnombre', $param)
+            and array_key_exists('uspass', $param) and array_key_exists('usmail', $param)
+            and array_key_exists('usdeshabilitado', $param)) {
             $obj = new Usuario();
-            $obj->setear($param['id'], $param['descript']);
+            $obj->setear(
+                $param['idusuario'],
+                $param['usnombre'],
+                $param['uspass'],
+                $param['usmail'],
+                $param['usdeshabilitado']
+            );
         }
         return $obj;
     }
-    
+
     /**
      * Espera como parametro un arreglo asociativo donde las claves coinciden con los nombres de las variables instancias del objeto que son claves
      * @param array $param
      * @return Usuario
      */
-    private function cargarObjetoConClave($param){
+    private function cargarObjetoConClave($param)
+    {
         $obj = null;
-        
-        if( isset($param['idusuario']) ){
+
+        if (isset($param['idusuario'])) {
             $obj = new Usuario();
-            $obj->setear($param['idusuario'],"","","",null);
+            $obj->setear($param['idusuario'], "", "", "", null);
         }
         return $obj;
     }
-    
-    
+
     /**
      * Corrobora que dentro del arreglo asociativo estan seteados los campos claves
      * @param array $param
      * @return boolean
      */
-    
-    private function seteadosCamposClaves($param){
+
+    private function seteadosCamposClaves($param)
+    {
         $resp = false;
-        if (isset($param['id']))
+        if (isset($param['idusuario'])) {
             $resp = true;
+        }
+
         return $resp;
     }
-    
+
     /**
-     * 
+     *
      * @param array $param
      */
-    public function alta($param){
+    public function alta($param)
+    {
         $resp = false;
-        $param['id'] =null;
-        $elObjtTabla = $this->cargarObjeto($param);
-//        verEstructura($elObjtTabla);
-        if ($elObjtTabla!=null and $elObjtTabla->insertar()){
+        $param['idusuario'] = null;
+        $elObjtUsuario = $this->cargarObjeto($param);
+        if ($elObjtUsuario != null and $elObjtUsuario->insertar()) {
             $resp = true;
         }
         return $resp;
-        
+
     }
     /**
-     * permite eliminar un objeto 
+     * permite eliminar un objeto
      * @param array $param
      * @return boolean
      */
-    public function baja($param){
+    public function baja($param)
+    {
         $resp = false;
-        if ($this->seteadosCamposClaves($param)){
-            $elObjtTabla = $this->cargarObjetoConClave($param);
-            if ($elObjtTabla!=null and $elObjtTabla->eliminar()){
+        if ($this->seteadosCamposClaves($param)) {
+            $elObjtUsuario = $this->cargarObjetoConClave($param);
+            if ($elObjtUsuario != null and $elObjtUsuario->eliminar()) {
                 $resp = true;
             }
         }
-        
         return $resp;
     }
-    
+
     /**
      * permite modificar un objeto
      * @param array $param
      * @return boolean
      */
-    public function modificacion($param){
-        //echo "Estoy en modificacion";
+    public function modificacion($param)
+    {
         $resp = false;
-        if ($this->seteadosCamposClaves($param)){
-            $elObjtTabla = $this->cargarObjeto($param);
-            if($elObjtTabla!=null and $elObjtTabla->modificar()){
+        if ($this->seteadosCamposClaves($param)) {
+            $elObjtUsuario = $this->cargarObjeto($param);
+            if ($elObjtUsuario != null and $elObjtUsuario->modificar()) {
                 $resp = true;
             }
         }
         return $resp;
     }
-    
+
     /**
      * permite buscar un objeto
      * @param array $param
      * @return boolean
      */
-    public function buscar($param){
+    public function buscar($param)
+    {
         $where = " true ";
-        if ($param<>NULL){
-            if  (isset($param['idusuario']))
-                $where.=" and idusuario =".$param['idusuario'];
-            if  (isset($param['usnombre']))
-                 $where.=" and usnombre ='".$param['usnombre']."'";
-            if  (isset($param['uspass']))
-            $where.=" and uspass ='".$param['uspass']."'";
-            if  (isset($param['usmail']))
-            $where.=" and usmail ='".$param['usmail']."'";
-            if  (isset($param['usdeshabilitado']))
-            $where.=" and usdeshabilitado ='".$param['usdeshabilitado']."'";
+        if ($param != null) {
+            if (isset($param['idusuario'])) {
+                $where .= " and idusuario =" . $param['idusuario'];
+            }
+
+            if (isset($param['usnombre'])) {
+                $where .= " and usnombre ='" . $param['usnombre'] . "'";
+            }
+
+            if (isset($param['uspass'])) {
+                $where .= " and uspass ='" . $param['uspass'] . "'";
+            }
+
+            if (isset($param['usmail'])) {
+                $where .= " and usmail ='" . $param['usmail'] . "'";
+            }
+
+            if (isset($param['usdeshabilitado'])) {
+                $where .= " and usdeshabilitado ='" . $param['usdeshabilitado'] . "'";
+            }
+
         }
-        $arreglo = Usuario::listar($where);  
+        $arreglo = Usuario::listar($where);
         return $arreglo;
-            
-            
-      
-        
+
     }
 
 }
-
-?>
