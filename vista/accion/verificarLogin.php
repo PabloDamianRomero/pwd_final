05c8bfd5204c356f1e51024c98ea010c87b87aee
Pablo Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-09 05:28:53 -0300

    script provisional para verificarLogin

--- vista/accion/verificarLogin.php
+++ vista/accion/verificarLogin.php
@@ -2,14 +2,15 @@
     include_once("../../configuracion.php");
     $datos = data_submitted();
     $objSesion = new Session();
-    $objSesion->iniciar($datos['usnombre'],md5($datos['uspass']));
+    // $objSesion->iniciar($datos['usnombre'],md5($datos['uspass']));
+    $objSesion->iniciar($datos['usnombre'], $datos['uspass']);
     $valido = $objSesion->validar();
     if ($valido){
         header("Location:../index.php");
         exit();
     }else{
         $msj = $objSesion->getMensajeError();
-        $objSesion->cerrar();
+        // $objSesion->cerrar(); // no haría falta, ya que cierra sesión en metodo validar
         header("Location:../login.php?error=$msj");
         exit();
     }
