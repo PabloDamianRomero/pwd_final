46b8110f45396b8256e3ea072df38b73f8c89eed
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-12 06:18:11 -0300

    agregado nombre de tienda

--- vista/paginaSegura.php
+++ vista/paginaSegura.php
@@ -6,14 +6,12 @@ include_once "estructura/cabecera.php";
 ?>
 
 <div style="margin-bottom: 20%" class="container-fluid text-center">
-    <div style="margin-top: 30px;">
-        <h1>Componentes de Pc</h1>
+    <div class="jumbotron jumbotron-fluid" style="margin-top: 30px;">
+        <div class="container">
+            <h1 class="display-4">Compupartes</h1>
+            <p class="lead">La tienda online de componentes de PC que estabas buscando.</p>
+        </div>
     </div>
-    <pre>
-        <?php
-        print_r($_SESSION);
-        ?>
-    </pre>
 <div>
 
 <?php
