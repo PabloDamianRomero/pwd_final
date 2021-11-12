5abf3ad058fca5f6fe94e9fc031ef118974f841c
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-12 04:55:56 -0300

    agregada nueva cabecera a index (cuando no se está logueado)

--- vista/index.php
+++ vista/index.php
@@ -2,12 +2,12 @@
 $titulo = "Index";
 $estructuraAMostrar = "desdeVista";
 $seguro = true;
-include_once "estructura/cabecera.php";
+include_once "estructura/cabecera1.php";
 ?>
 
 <div style="margin-bottom: 20%" class="container-fluid text-center">
     <div style="margin-top: 30px;">
-        <h1>Bienvenid@ <?php echo $sesion->getUsuarioActual(); ?></h1>
+        <h1>Componentes de Pc</h1>
     </div>
 <div>
 
