7625fc10011d0d6c29c055f9e88361bad5b4c21a
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-19 07:05:23 -0300

    agregado uso de fontawesome

--- vista/estructura/cabecera.php
+++ vista/estructura/cabecera.php
@@ -6,6 +6,7 @@ if ($estructuraAMostrar == "desdeVista") {
     include_once("../configuracion.php");?>
     <link rel='stylesheet' href='css/bootstrap/bootstrap.css'>
     <link rel='stylesheet' href='css/bootstrap/bootstrap.min.css'>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <!-- Enlaces jQuery-Easyui -->
     <link rel="stylesheet" type="text/css" href="../util/jquery-easyui/themes/default/easyui.css">
     <link rel="stylesheet" type="text/css" href="../util/jquery-easyui/themes/icon.css">
