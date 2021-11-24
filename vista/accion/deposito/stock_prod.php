dd39cf58c31624c945a39c57b400b4b7aac65346
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-24 18:20:24 -0300

    controlado valor de stock al modificar (no negativo)

--- vista/accion/deposito/stock_prod.php
+++ vista/accion/deposito/stock_prod.php
@@ -4,7 +4,16 @@ $datos=data_submitted();
 
 if (isset($datos['idproducto']) && isset($datos['pronombre']) && isset($datos['prodetalle']) && isset($datos['proprecio']) && isset($datos['procantstock']) && isset($datos['prodeshabilitado'])){
     $abmProd=new abmProducto();
-    $resp=$abmProd->modificacion($datos);
+    $controlCantidad = true;
+    if($datos['procantstock'] < 0){
+        $controlCantidad = false;
+    }
+    if($controlCantidad){
+        $resp=$abmProd->modificacion($datos);
+    }else{
+        $resp=false;
+        $retorno['errorMsg']="El valor de stock no puede ser negativo. Si no existen productos coloque 0 (Cero).";
+    }
 }else{
     $resp=false;
     $retorno['errorMsg']="No se pudo MODIFICAR el stock.";
