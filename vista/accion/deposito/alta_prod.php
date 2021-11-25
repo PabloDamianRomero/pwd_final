096f8f016d75dc8984ec99047c2cb99e68f4ecce
Pablo Damian Romero <pablo.romero@est.fi.uncoma.edu.ar>
2021-11-25 10:28:44 -0300

    código comentado eliminado

--- vista/accion/deposito/alta_prod.php
+++ vista/accion/deposito/alta_prod.php
@@ -2,19 +2,6 @@
     include_once("../../../configuracion.php");
     $datos=data_submitted();
 
-    // include_once("../../../modelo/conector/BaseDatos.php");
-    // include_once("../../../modelo/Rol.php");
-    // include_once("../../../modelo/UsuarioRol.php");
-    // include_once("../../../modelo/Usuario.php");
-    // include_once("../../../modelo/Menurol.php");
-    // include_once("../../../modelo/Producto.php");
-    // include_once("../../../control/abmRol.php");
-    // include_once("../../../control/abmUsuariorol.php");
-    // include_once("../../../control/abmUsuario.php");
-    // include_once("../../../control/abmMenurol.php");
-    // include_once("../../../control/abmProducto.php");
-    // $datos=['pronombre'=>'Placa de video Nvidia MSI GeForce 10 Series GT 1030 GEFORCE GT 1030 2GD4 LP OC OC Edition 2GB','proprecio'=>'23.479'];
-
     if (isset($datos['pronombre']) && isset($datos['proprecio'])){
         $abmProd=new abmProducto();
         $colProd=$abmProd->buscar(null);
